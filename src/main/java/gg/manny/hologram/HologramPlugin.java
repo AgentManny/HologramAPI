c547558c719f986076ecf940c486151b069a48d9
AgentManny <itsreallymanny@gmail.com>
2021-11-01 16:26:30 +0000

    Added plugin.yml

--- src/main/java/gg/manny/hologram/HologramPlugin.java
+++ src/main/java/gg/manny/hologram/HologramPlugin.java
@@ -1,4 +1,22 @@
 package gg.manny.hologram;
 
-public class HologramPlugin {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HologramPlugin extends JavaPlugin {
+
+    private static HologramPlugin instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+    }
+
+    @Override
+    public void onDisable() {
+        instance = null;
+    }
+
+    public static HologramPlugin getInstance() {
+        return instance;
+    }
 }
